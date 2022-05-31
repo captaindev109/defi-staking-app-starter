25d2f0d3db1f63154e871be46b64c894517d88af
captaindev <ismailrus109@gmail.com>
2022-05-31 06:44:06 -0500

    running migration

--- defi-staking-app-starter/contracts/DecentralBank.sol
+++ defi-staking-app-starter/contracts/DecentralBank.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Tether {
   string public name = 'Tether';
-  string public symbol = 'USDT';
+  string public symbol = 'mUSDT';
   uint256 totalSupply = 1000000000000000000000000; // 1 million tokens
   uint8 public decimals = 18;
 }
