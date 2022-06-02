ad6b1bc32ea31d6eab88aae99c4350f4ceb4c844
captaindev <ismailrus109@gmail.com>
2022-06-02 12:49:48 +0900

    updated

--- defi-staking-app-starter/contracts/DecentralBank.sol
+++ defi-staking-app-starter/contracts/DecentralBank.sol
@@ -1,6 +1,16 @@
 pragma solidity >=0.4.22 <0.9.0;
+import './RWD.sol';
+import './Tether.sol';
 
-contract Tether {
+contract DecentralBank {
   string public name = "Decentral Bank";
   address public owner;
+
+  RWD public rwd;
+  Tether public tether;
+
+  constructor(RWD _rwd, Tether _tether) {
+    rwd = _rwd;
+    tether = _tether;
+  }
 }
