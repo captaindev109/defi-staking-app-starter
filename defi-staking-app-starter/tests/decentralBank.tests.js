b87a8ace863ab7c64ee2aa59e253b0d248ba45b9
captaindev <ismailrus109@gmail.com>
2022-06-03 18:53:04 +0900

    chai test

--- defi-staking-app-starter/tests/decentralBank.tests.js
+++ defi-staking-app-starter/tests/decentralBank.tests.js
@@ -9,11 +9,24 @@ require('chai')
 .should()
 
 contract('DecentralBank', (accounts) => {
+  let tether, rwd
+
+  before(async() => {
+    tether = await Tether.new()
+    rwd = await RWD.new()
+  })
+
   describe('Mock Tether Deployment', async () => {
     it('matches name successfully', async () => {
-      let tether = await Tether.new()
-      const name = tether.name()
+      const name = await tether.name()
       assert.equal(name, 'Mock Tether Token')
+    })
+  })
+  
+  describe('Reward Token', async () => {
+    it('matches name successfully', async () => {
+      const name = await rwd.name()
+      assert.equal(name, 'Reward Token')
     })
   })
 })
