96dca7c3296343c75df9afd7d6052cc64ff02d99
captaindev <ismailrus109@gmail.com>
2022-06-06 11:39:11 +0900

    add web3 connection

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react'
 import Navbar from './Navbar';
+import Web3 from 'web3';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x00'
+    }
+  }
+
+  async componentWillUnmount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const account = web3.eth.getAccounts()
+    console.log(account)
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('No ethereum browser detected! Check out Metamask!')
+    }
+  }
+
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar account={this.state.account} />
         <div className='text-center'>
-          <h>Hello, world!</h>
+          <h1>Hello, world!</h1>
         </div>
       </div>
     )
